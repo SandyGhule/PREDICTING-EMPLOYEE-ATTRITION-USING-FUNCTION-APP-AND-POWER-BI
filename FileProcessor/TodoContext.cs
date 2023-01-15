d6f3a3ada4f5cf049cc85d8b51052bbb7f622f1c
Crossfire <73627180+SandyGhule@users.noreply.github.com>
2023-01-15 18:38:41 +0530

    Add files via upload

---
+++ FileProcessor/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using FileProcessor.Classes;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace FileProcessor
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("TodoContext")
+        {
+        }
+
+        public DbSet<EmpData> Emp_Prediction { get; set; }
+
+    }
+}
